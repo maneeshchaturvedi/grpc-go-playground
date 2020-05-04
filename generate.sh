9c063ec9d173b792bf29c6d2dbcfc2d7d88f66e7
Maneesh Chaturvedi <maneesh.chaturvedi@myntra.com>
2020-05-04 15:40:26 +0530

    Added commands to generate.sh

--- generate.sh
+++ generate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 protoc unary/greet/greetpb/greet.proto --go_out=plugins=grpc:.
 protoc unary/calculator/calculatorpb/calculator.proto --go_out=plugins=grpc:.
+protoc server_streaming/greet/greetpb/greet.proto --go_out=plugins=grpc:.
+protoc server_streaming/calculator/calculatorpb/calculator.proto --go_out=plugins=grpc:.
